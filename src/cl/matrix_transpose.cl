26d80c70e1ce45a795e9b0aea7e8704a19db3d16
Ekaterina Tochilina <tochilina.2002@yandex.ru>
2023-09-30 19:18:24 +0300

    transpose

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,25 @@
-__kernel void matrix_transpose(...)
+#define TILE_SIZE 32
+
+__kernel void matrix_transpose(__global float *a, __global float *at, unsigned int m, unsigned int k)
 {
-    // TODO
+    int i = get_global_id(0);
+    int j = get_global_id(1);
+    __local float tile[TILE_SIZE * TILE_SIZE];
+    int local_i = get_local_id(0);
+    int local_j = get_local_id(1);
+    int i0 = i - local_i;
+    int j0 = j - local_j;
+
+    if (i < k && j < m) {
+        tile[local_j * TILE_SIZE + local_i] = a[j * k + i];
+    //} else {
+    //    tile[local_j * TILE_SIZE + local_i] = 0;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+    float value = tile[local_i * TILE_SIZE + local_j];
+    barrier(CLK_LOCAL_MEM_FENCE);
+    tile[local_j * TILE_SIZE + local_i] = value;
+
+    if (j0 + local_i < m && i0 + local_j < k)
+        at[(i0 + local_j) * m + j0 + local_i] = tile[local_j * TILE_SIZE + local_i];
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -32,7 +32,6 @@ int main(int argc, char **argv)
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << std::endl;
 
-    /*
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
@@ -46,14 +45,17 @@ int main(int argc, char **argv)
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
+             unsigned int workGroupSizeX = 16;
+            unsigned int workGroupSizeY = 16;
+            unsigned int workSizeX = (K + workGroupSizeX - 1) / workGroupSizeX * workGroupSizeX;
+            unsigned int workSizeY = (M + workGroupSizeY - 1) / workGroupSizeY * workGroupSizeY;
+            auto workSize = gpu::WorkSize(workGroupSizeX, workGroupSizeY, workSizeX, workSizeY);
             // Для этой задачи естественнее использовать двухмерный NDRange. Чтобы это сформулировать
             // в терминологии библиотеки - нужно вызвать другую вариацию конструктора WorkSize.
             // В CLion удобно смотреть какие есть вариант аргументов в конструкторах:
             // поставьте каретку редактирования кода внутри скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4 и 6 параметров
             // - для 1D, 2D и 3D рабочего пространства соответственно
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
+            matrix_transpose_kernel.exec(workSize, as_gpu, as_t_gpu, M, K);
 
             t.nextLap();
         }
@@ -74,7 +76,6 @@ int main(int argc, char **argv)
             }
         }
     }
-    */
 
     return 0;
 }
